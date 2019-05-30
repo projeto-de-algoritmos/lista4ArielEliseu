c3432cb9305a3c546cb00c3ce09506759aebf9bb
ariel <arielserafi@gmail.com>
2019-05-30 17:51:09 -0300

    Problema resolvido devido a: https://www.ardanlabs.com/blog/2014/03/exportedunexported-identifiers-in-go.html

--- knowledgecloud/Body.go
+++ knowledgecloud/Body.go
@@ -6,14 +6,14 @@ Based on the behavior determined on the Head the Samples are analyzed in order t
 The analysis is explored in the interface file.
 */
 type DataBody struct {
-	ranks []int //samples ranks
+	Ranks []int //samples ranks
 
-	idealrankscenter []int //ideal "center of the ranks" for each label
-	realrankcenter   []int //real "center of the ranks" for each label
+	Idealrankscenter []int //ideal "center of the ranks" for each label
+	Realrankcenter   []int //real "center of the ranks" for each label
 
-	idealpoints []int //ideal center of mass
-	realpoints  []int //real center of mass
+	Idealpoints []int //ideal center of mass
+	Realpoints  []int //real center of mass
 
-	distrank    []int //results based on ranks
-	distclassic []int //results based on points
+	Distrank    []int //results based on ranks
+	Distclassic []int //results based on points
 }
--- main.go
+++ main.go
@@ -7,16 +7,17 @@ import (
 
 func main() {
 	A := []int{1, 2, 3, 4}
+
 	B := []int{10, 20, 30, 40}
+	C := []int{-10, -20, -30, -40}
 
-	C := []int{-1, -2, -3, -4}
-	D := []int{-10, -20, -30, -40}
+	D := []int{-1, -2, -3, -4}
+	E := []int{-10, -20, -30, -40}
 
-	E := []int{100, 200, 300, 400}
-	F := []int{-100, -200, -300, -400}
+	F := []int{100, 200, 300, 400}
 
-	//fmt.Println(DataHead{true, 1, 2, 3, 4})
-	//fmt.Println(DataBody{A, B, C, D, E, F})
+	//fmt.Println(knowledgecloud.DataHead{true, 1, 2, 3, 3, 4})
+	//fmt.Println(knowledgecloud.DataBody{A, B, C, D, E, F, G})
 
 	h := knowledgecloud.DataHead{true, 1, 2, 3, 3, 4}
 	b := knowledgecloud.DataBody{A, B, B, C, D, E, F}
